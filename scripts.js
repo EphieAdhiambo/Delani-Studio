c800d8c2701a990b8c2c4ae1278a986203204ed6
Ephie Adhiambo <ephie.adhiambo@student.moringaschool.com>
2021-07-25 22:41:37 +0300

    add design icon

--- scripts.js
+++ scripts.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
-    $(".design-icon").click(function){
-        $(".design-icon").slideToggle();
-        $(".design").slideToggle();
-    });
-});
+ $(".design-icon").click(function(){
+     $(".design-icon").slideToggle();
+     $(".design").slideToggle();
+})
+
+
+})
+$(".design").click(function(){
+    $(".design").slideTogglez();
+    $(".design-icon").slideToggle();
+})

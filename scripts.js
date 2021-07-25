7d7cf112fba771064f6e8535aa4ca982e49b53e5
Ephie Adhiambo <ephie.adhiambo@student.moringaschool.com>
2021-07-26 02:05:51 +0300

    add files to the portfolio

--- scripts.js
+++ scripts.js
@@ -19,82 +19,140 @@ $(document).ready(function(){
 // });
 // });
 
+// $(document).ready(function(){
+//     $("#development-image").click(function(){
+//       $("#development-image").slideDown('1500').hide('1000');
+//       $("#development").show('1500');
+//     });
+//     $("#development").click(function(){
+//       $("#development").slideUp('1500');
+//       $("#development-image").slideDown('1500');
+//     });
+//   });
+
+//   $(document).ready(function(){
+//     $("#product-image").click(function(){
+//       $("#product-image").slideDown('1500').hide('1000');
+//       $("#product").show('1500');
+//     });
+//     $("#product").click(function(){
+//       $("#product").slideUp('1500');
+//       $("#product-image").slideDown('1500');
+//     });
+//   });
+
+//   $(document).ready(function(){
+//     $("#work1").mouseover(function(){
+//       $("#overlay").show();
+//     }).mouseout(function(){
+//       $("#overlay").hide();
+//     });
+//   });
+  
+//   $(document).ready(function(){
+//     $("#work2").mouseover(function(){
+//       $("#overlay2").show();
+//     }).mouseout(function(){
+//       $("#overlay2").hide();
+//     });
+//   });
+  
+//   $(document).ready(function(){
+//     $("#work3").mouseover(function(){
+//       $("#overlay3").show();
+//     }).mouseout(function(){
+//       $("#overlay3").hide();
+//     });
+//   });
+  
+//   $(document).ready(function(){
+//     $("#work4").mouseover(function(){
+//       $("#overlay4").show();
+//     }).mouseout(function(){
+//       $("#overlay4").hide();
+//     });
+//   });
+  
+//   $(document).ready(function(){
+//     $("#work5").mouseover(function(){
+//       $("#overlay5").show();
+//     }).mouseout(function(){
+//       $("#overlay5").hide();
+//     });
+  
+//     $("#work6").mouseover(function(){
+//       $("#overlay6").show();
+//     }).mouseout(function(){
+//       $("#overlay6").hide();
+//     });
+  
+//     $("#work7").mouseover(function(){
+//       $("#overlay7").show();
+//     }).mouseout(function(){
+//       $("#overlay7").hide();
+//     });
+  
+//     $("#work8").mouseover(function(){
+//       $("#overlay8").show();
+//     }).mouseout(function(){
+//       $("#overlay8").hide();
+//     });
+//   });
 $(document).ready(function(){
-    $("#development-image").click(function(){
-      $("#development-image").slideDown('1500').hide('1000');
-      $("#development").show('1500');
+    $("#work").mouseover(function(){
+    $("#mouseenter").show();
+     });
+    $("#work").mouseout(function(){
+    $("#mouseenter").hide();
     });
-    $("#development").click(function(){
-      $("#development").slideUp('1500');
-      $("#development-image").slideDown('1500');
-    });
-  });
-
-  $(document).ready(function(){
-    $("#product-image").click(function(){
-      $("#product-image").slideDown('1500').hide('1000');
-      $("#product").show('1500');
-    });
-    $("#product").click(function(){
-      $("#product").slideUp('1500');
-      $("#product-image").slideDown('1500');
-    });
-  });
-
-  $(document).ready(function(){
-    $("#work1").mouseover(function(){
-      $("#overlay").show();
-    }).mouseout(function(){
-      $("#overlay").hide();
-    });
-  });
   
-  $(document).ready(function(){
+
+  $("#work1").mouseover(function(){
+      $("#mouseenter1").show();
+    })
+    $("#work1").mouseout(function(){
+      $("#mouseenter1").hide();
+    });
+   
     $("#work2").mouseover(function(){
-      $("#overlay2").show();
-    }).mouseout(function(){
-      $("#overlay2").hide();
+      $("#mouseenter2").show();
+    })
+    $("#work2").mouseout(function(){
+      $("#mouseenter2").hide();
     });
-  });
-  
-  $(document).ready(function(){
+    
     $("#work3").mouseover(function(){
-      $("#overlay3").show();
-    }).mouseout(function(){
-      $("#overlay3").hide();
+      $("#mouseenter3").show();
+    })
+    $("#work3").mouseout(function(){
+      $("#mouseenter3").hide();
     });
-  });
-  
-  $(document).ready(function(){
+    
     $("#work4").mouseover(function(){
-      $("#overlay4").show();
-    }).mouseout(function(){
-      $("#overlay4").hide();
+      $("#mouseenter4").show();
+    })
+    $("#work4").mouseout(function(){
+      $("#mouseenter4").hide();
     });
-  });
-  
-  $(document).ready(function(){
+      
     $("#work5").mouseover(function(){
-      $("#overlay5").show();
-    }).mouseout(function(){
-      $("#overlay5").hide();
+      $("#mouseenter5").show();
+    })
+    $("#work5").mouseout(function(){
+      $("#mouseenter5").hide();
     });
-  
+     
     $("#work6").mouseover(function(){
-      $("#overlay6").show();
-    }).mouseout(function(){
-      $("#overlay6").hide();
+      $("#mouseenter6").show();
+    })
+    $("#work6").mouseout(function(){
+      $("#mouseenter6").hide();
     });
-  
+    
     $("#work7").mouseover(function(){
-      $("#overlay7").show();
-    }).mouseout(function(){
-      $("#overlay7").hide();
+      $("#mouseenter7").show();
+    })
+    $("#work7").mouseout(function(){
+      $("#mouseenter7").hide();
     });
-  
-    $("#work8").mouseover(function(){
-      $("#overlay8").show();
-    }).mouseout(function(){
-      $("#overlay8").hide();
-    });
-  });
+});

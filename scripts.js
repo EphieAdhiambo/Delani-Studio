4f077eb87909662aa7ea958285d7d97f213a54b5
Ephie Adhiambo <ephie.adhiambo@student.moringaschool.com>
2021-07-26 10:27:55 +0300

    submit button

--- scripts.js
+++ scripts.js
@@ -101,19 +101,15 @@ $(document).ready(function(){
     });
 });
 
-function myMessage() {
+function sendMessage() {
   var name = document.getElementById("name").value;
   var email = document.getElementById("email").value;
          var message = document.getElementById("comment").value;
  if (email !== "" && name !== "" && message !== "") {
-   // document.getElementById("pop").innerHTML =
    alert(
-     "Hello" +
-       " " +
-       name +
-       " we have received your message. Thank you for reaching out to us."
+     "Thank you,we have received your message."
    );
  } else if (email == "" || name !== "" || message !== "") {
-   alert("Please fill in all the details");
+   alert("Put in your details below");
  }
 }

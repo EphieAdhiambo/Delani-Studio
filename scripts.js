5efa9ea275c44511988b1eb14e098522debdc54e
Ephie Adhiambo <ephie.adhiambo@student.moringaschool.com>
2021-07-26 10:16:46 +0300

    contact us section

--- scripts.js
+++ scripts.js
@@ -19,85 +19,29 @@ $(document).ready(function(){
 // });
 // });
 
-// $(document).ready(function(){
-//     $("#development-image").click(function(){
-//       $("#development-image").slideDown('1500').hide('1000');
-//       $("#development").show('1500');
-//     });
-//     $("#development").click(function(){
-//       $("#development").slideUp('1500');
-//       $("#development-image").slideDown('1500');
-//     });
-//   });
+$(document).ready(function(){
+    $("#development-image").click(function(){
+      $("#development-image").slideDown('1500').hide('1000');
+      $("#development").show('1500');
+    });
+    $("#development").click(function(){
+      $("#development").slideUp('1500');
+      $("#development-image").slideDown('1500');
+    });
+  });
 
-//   $(document).ready(function(){
-//     $("#product-image").click(function(){
-//       $("#product-image").slideDown('1500').hide('1000');
-//       $("#product").show('1500');
-//     });
-//     $("#product").click(function(){
-//       $("#product").slideUp('1500');
-//       $("#product-image").slideDown('1500');
-//     });
-//   });
+  $(document).ready(function(){
+    $("#product-image").click(function(){
+      $("#product-image").slideDown('1500').hide('1000');
+      $("#product").show('1500');
+    });
+    $("#product").click(function(){
+      $("#product").slideUp('1500');
+      $("#product-image").slideDown('1500');
+    });
+  });
 
-//   $(document).ready(function(){
-//     $("#work1").mouseover(function(){
-//       $("#overlay").show();
-//     }).mouseout(function(){
-//       $("#overlay").hide();
-//     });
-//   });
-  
-//   $(document).ready(function(){
-//     $("#work2").mouseover(function(){
-//       $("#overlay2").show();
-//     }).mouseout(function(){
-//       $("#overlay2").hide();
-//     });
-//   });
-  
-//   $(document).ready(function(){
-//     $("#work3").mouseover(function(){
-//       $("#overlay3").show();
-//     }).mouseout(function(){
-//       $("#overlay3").hide();
-//     });
-//   });
-  
-//   $(document).ready(function(){
-//     $("#work4").mouseover(function(){
-//       $("#overlay4").show();
-//     }).mouseout(function(){
-//       $("#overlay4").hide();
-//     });
-//   });
-  
-//   $(document).ready(function(){
-//     $("#work5").mouseover(function(){
-//       $("#overlay5").show();
-//     }).mouseout(function(){
-//       $("#overlay5").hide();
-//     });
-  
-//     $("#work6").mouseover(function(){
-//       $("#overlay6").show();
-//     }).mouseout(function(){
-//       $("#overlay6").hide();
-//     });
-  
-//     $("#work7").mouseover(function(){
-//       $("#overlay7").show();
-//     }).mouseout(function(){
-//       $("#overlay7").hide();
-//     });
-  
-//     $("#work8").mouseover(function(){
-//       $("#overlay8").show();
-//     }).mouseout(function(){
-//       $("#overlay8").hide();
-//     });
-//   });
+
 $(document).ready(function(){
     $("#work").mouseover(function(){
     $("#mouseenter").show();
@@ -156,3 +100,20 @@ $(document).ready(function(){
       $("#mouseenter7").hide();
     });
 });
+
+function myMessage() {
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+         var message = document.getElementById("comment").value;
+ if (email !== "" && name !== "" && message !== "") {
+   // document.getElementById("pop").innerHTML =
+   alert(
+     "Hello" +
+       " " +
+       name +
+       " we have received your message. Thank you for reaching out to us."
+   );
+ } else if (email == "" || name !== "" || message !== "") {
+   alert("Please fill in all the details");
+ }
+}
